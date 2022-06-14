e7bd155fc9470b37a5a710de6ddf257dd1513058
Max Park <itsmaxpark@gmail.com>
2022-06-14 16:34:39 -0400

    Initial Commit

---
+++ Github Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Followers
+//
+//  Created by Max Park on 6/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
